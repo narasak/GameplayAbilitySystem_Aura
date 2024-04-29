f750e6b61fd9ccf930de7d18318cecd0a2d7b3b0
Narasak Mansurang <narasak.man@gmail.com>
2024-04-29 13:18:37 +0700

    Refactor AuraCharacterBase and create other character classes
    The old AuraCharacterBase has been deleted and refactored into a smaller file under a new directory. The functionality to tick every frame has been removed to improve performance. Furthermore, two new subclasses of AuraCharacterBase have been created: AuraCharacter and AuraEnemy.

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 27Antz
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
