dc90023bdd84db13b840bb8ee6744853bf5fb062
Narasak Mansurang <narasak.man@gmail.com>
2024-04-29 17:08:32 +0700

    Add weapon attribute to AuraCharacterBase and implement EnhancedInput
    A weapon attribute has been added to the AuraCharacterBase class, including its setup on the BeginPlay override. EnhancedInput has been implemented in Aura by adding it as a public dependency in Aura.Build.cs and creating the AuraPlayerController class. The new class object manages the input context and adds mapping on BeginPlay.

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,26 @@
+// Copyright 27Antz
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputMappingContext.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+};
