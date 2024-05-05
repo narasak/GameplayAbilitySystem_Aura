e5162f5d11d93daf6f4fa5cda4e2df184cd0bebf
Narasak Mansurang <narasak.man@gmail.com>
2024-05-05 20:41:55 +0700

    Add BroadcastInitialValues in WidgetControllers
    This commit adds a new function called BroadcastInitialValues in both AuraWidgetController and OverlayWidgetController. In OverlayWidgetController, it's used to broadcast initial health values. Also, it introduces UObject pointers to AuraPlayerController for LastActor and ThisActor.

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,13 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AttributeSet.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
